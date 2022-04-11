79b9b7f1b04234f784f828bc57b0440e16916fcd
anjh11 <103414628+anjh11@users.noreply.github.com>
2022-04-11 16:24:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var icon = document.getElementById("icon");
+var header = document.getElementById("header");
+header.addEventListener("click", () => {
+    location.replace("index.html");
+})
+
+var select_left = document.getElementById("select_left");
+select_left.addEventListener("click", () => {
+    console.log("cuccess!");
+})
